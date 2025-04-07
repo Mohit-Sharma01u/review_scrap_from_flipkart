eefbb6c6827c6c562682d68abfeecbd3c2c46af0
Mohit-Sharma01u <mohitsharma01u@gmail.com>
2025-04-07 22:19:18 +0530

    find out the reviews

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 import logging
+import pymongo
+
 logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
 
 app = Flask(__name__)
@@ -22,15 +24,19 @@ def index():
             flipkartPage = uClient.read()
             uClient.close()
             flipkart_html = bs(flipkartPage, "html.parser")
-            bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})
-            del bigboxes[0:3]
+            # bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})
+            bigboxes = flipkart_html.findAll("a", {"class":"CGtC98"})
+            # del bigboxes[0:3]
             box = bigboxes[0]
-            productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
+            productLink = "https://www.flipkart.com" + box["href"]
+
+
             prodRes = requests.get(productLink)
             prodRes.encoding='utf-8'
+            
             prod_html = bs(prodRes.text, "html.parser")
             print(prod_html)
-            commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})
+            commentboxes = prod_html.find_all("div", {"class":"col EPCmJX"})
 
             filename = searchString + ".csv"
             fw = open(filename, "w")
@@ -40,14 +46,14 @@ def index():
             for commentbox in commentboxes:
                 try:
                     #name.encode(encoding='utf-8')
-                    name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
+                    name = commentbox.find_all("p", {"class":"_2NsDsF AwS1CA"})[0].text
 
                 except:
                     logging.info("name")
 
                 try:
                     #rating.encode(encoding='utf-8')
-                    rating = commentbox.div.div.div.div.text
+                    rating = commentbox.div.div.text
 
 
                 except:
@@ -56,15 +62,18 @@ def index():
 
                 try:
                     #commentHead.encode(encoding='utf-8')
-                    commentHead = commentbox.div.div.div.p.text
+                    commentHead = commentbox.div.p.text
 
                 except:
                     commentHead = 'No Comment Heading'
                     logging.info(commentHead)
+
+
+
                 try:
-                    comtag = commentbox.div.div.find_all('div', {'class': ''})
+                    comtag = commentbox.find_all("div", {"class":""})[0].div.text
                     #custComment.encode(encoding='utf-8')
-                    custComment = comtag[0].div.text
+                    custComment = comtag
                 except Exception as e:
                     logging.info(e)
 
@@ -72,6 +81,12 @@ def index():
                           "Comment": custComment}
                 reviews.append(mydict)
             logging.info("log my final result {}".format(reviews))
+
+            client = pymongo.MongoClient("mongodb://localhost:27017/")
+            db = client['review_scrap']
+            review_col = db['review_data']
+            review_col.insert_many(reviews)
+
             return render_template('result.html', reviews=reviews[0:(len(reviews)-1)])
         except Exception as e:
             logging.info(e)
